f3ae589ac318a65cdbb587d3761425c0af0e8e94
lf94 <inbox@leefallat.ca>
2023-09-05 22:42:16 -0400

    Update sketching

--- examples/sketching.js
+++ examples/sketching.js
@@ -1,16 +1,17 @@
 // Demonstrate how to use lib/sketch.js
 
-const { deg, mm, saveAsSTL } = require("../index");
-const { sketch, polar } = require("../lib/sketch");
+const { cylinder, deg, mm, saveAsSTL } = require("../index");
+const { sketch, polar } = require("../lib/sketch2");
 
 const cubeSketched = sketch({ startPoint: [0, 0] })
-  .line(polar(6*mm, 90*deg))
-  .line(polar(6*mm, 0*deg))
-  .line(polar(6*mm, -90*deg))
-  .line(polar(6*mm, 180*deg))
+  .line(polar(5*mm, 90*deg))
+  .line(polar(5*mm, 0*deg))
+  .line(polar(5*mm, -90*deg))
+  .line(polar(5*mm, 180*deg))
   .done()
-  .extrudeZ(0, 6*mm);
+  .extrudeZ(0, 5*mm);
 
 const region = [10*mm, 10*mm, 10*mm];
-saveAsSTL(cubeSketched, [region.mul(-1), region], 0.1, "sketching.stl");
+console.log(cubeSketched.reify());
+saveAsSTL(cubeSketched, [region.mul(-1), region], 1.0, "sketching.stl");
 
